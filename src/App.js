60ea0fa4d0de51270e74d86c399e834dcce826a8
Lucile Vacquie <lucilevacquie@luciles-mbp.mynet>
2020-12-10 13:56:16 +0000

    finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+// const defaultText =
+//   '# This is a header\n## This is a sub header\nThis is a a [link](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)\n`This is inline code`\n <pre><code>var codeblock = "This is a code block"</code></pre> \n![Cute puppy image](https://cdn.images.express.co.uk/img/dynamic/128/590x/secondary/Puppy-names-772155.jpg)\n**This text is bolded**\n>This is a blockquote\n- This is a list item'
+// const defaultText = '# header ## subheader [link]() `code` <pre><code>block</code></pre> ![Pup]() 1. list **bold**'
+let defaultText = '# header\n'
+defaultText += '## subheader\n'
+defaultText += '[link](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)\n'
+defaultText += '> blockquote\n\n'
+defaultText += '`inline code`\n'
+defaultText += '**bold text**\n\n'
+defaultText += '    javascript\n\n'
+defaultText += '- item 1\n\n'
+defaultText += '![Cute puppy image](https://cdn.images.express.co.uk/img/dynamic/128/590x/secondary/Puppy-names-772155.jpg)\n'
+
+const MarkdownPrev = () => {
+
+  const [text, setText] = useState(defaultText)
+
+  window.marked.setOptions({
+    breaks: true
+  })
+
+  return(
+    <div id="wrapper">
+      <h1>Markdown Previewer</h1>
+      <textarea id="editor" onChange={e => setText(e.target.value)}>{text}</textarea>
+      <div id="preview" dangerouslySetInnerHTML={{__html: window.marked(text)}}></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default MarkdownPrev;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MarkdownPrev from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MarkdownPrev />
   </React.StrictMode>,
   document.getElementById('root')
 );
